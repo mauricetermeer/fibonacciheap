5e82a2a23ef01f4974a961af4935bcb9b23ca033
Maurice Termeer <maurice.termeer@philips.com>
2013-09-26 11:05:08 +0200

    removed unsupported implementation variants

--- fibonacciheap.cpp
+++ fibonacciheap.cpp
@@ -1,3 +1,21 @@
+/**
+ * Mutable Fibonacci Heap example / test / benchmark
+ *
+ * Copyright © 2013, Maurice Termeer
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <algorithm>
 #include <cassert>
 #include <chrono>
@@ -7,7 +25,7 @@
 #include <random>
 #include <vector>
 
-#include "fibonacciheap-ptr.hpp"
+#include "fibonacciheap.hpp"
 
 //#define TEST_BOOST
 #ifdef TEST_BOOST
